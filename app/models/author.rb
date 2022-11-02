2d026591dbdb40bbdbce1b5ad7f9e551fbc67f1b
Deryn Brown <Deryn.2017@gmail.com>
2022-11-02 09:16:41 -0500

    commit tree bbe42fc658ec352cb9befc22daabb12bc73f65e7

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true #all authors have a name, no two authors have same name
+    validates :phone_number, length: { is: 10 } #author phone numbers are exactly 10 digits
 end
